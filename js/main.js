4d3db0f69db87611dda79f7ef73371ff1d1b3acc
Omar <o.a.1027@outlook.com>
2021-10-14 20:28:03 -0600

    add simple weather app card to show vancouver weather

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+
+    //weather app card.
+
+    const weatherCard = $('.weather-card');
+
+    const key ='66aedf850e52872851f1e3d201a4d53c';
+    const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?q=vancouver&appid=${key}`;
+
+    $.ajax({
+        method: 'GET',
+        url: weatherURL
+
+    }).always(()=>{
+        console.log('idk');
+    }).done((data)=> {
+        
+        const description = data.list[0].weather[0].description;
+        const current = data.list[0].main.temp;
+        const high = data.list[0].main.temp_max;
+        const low = data.list[0].main.temp_min;
+
+        weatherCard.append(`
+        <div class='project-images w-overlay'>
+            <p class='forecast-degrees'>${Math.floor(current) - 272}°</p>
+        </div>
+        <div class='prjct-descrptn'>
+        <h3>${data.city.name}\'s Weather Today</h3>
+        <hr>
+        <p>Right now there is ${description} with a high of ${Math.floor(high) - 272}° and a low of ${Math.floor(low) - 272}°.</p>
+        <a href='https://openweathermap.org/' target='_blank'><span>Open Weather</span> &reg;</a>
+        </div>`);
+    }).fail((err)=> {
+        console.error(err);
+    })
+    
+});
